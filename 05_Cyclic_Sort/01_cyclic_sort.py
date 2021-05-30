ac396e3400a67b7ef8a13fc9869c7d5eb40a712a
shash873 <shash873@gmail.com>
2021-05-30 09:51:54 -0700

    Cyclic Sort

---
+++ 05_Cyclic_Sort/01_cyclic_sort.py
@@ -0,0 +1,20 @@
+# Time O(N) | Space O(1)
+def cyclic_sort(nums):
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]
+        else:
+            i += 1
+    return nums
+
+
+def main():
+    print(cyclic_sort([3, 1, 5, 4, 2]))
+    print(cyclic_sort([2, 6, 4, 3, 1, 5]))
+    print(cyclic_sort([1, 5, 6, 4, 3, 2]))
+
+
+if __name__ == '__main__':
+    main()
