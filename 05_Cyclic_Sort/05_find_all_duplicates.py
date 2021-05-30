ed8d8faaf4ecc6cba72a716d875c112a9d269103
shash873 <shash873@gmail.com>
2021-05-30 11:51:52 -0700

    Find all Duplicate Numbers

---
+++ 05_Cyclic_Sort/05_find_all_duplicates.py
@@ -0,0 +1,25 @@
+# Time O(N) | Space O(1)
+def find_all_duplicates(nums):
+    i = 0
+    while i < len(nums):
+        j = nums[i] - 1
+        if nums[i] != nums[j]:
+            nums[i], nums[j] = nums[j], nums[i]  # swap
+        else:
+            i += 1
+
+    duplicate_numbers = []
+    for i in range(len(nums)):
+        if nums[i] != i + 1:
+            duplicate_numbers.append(nums[i])
+
+    return duplicate_numbers
+
+
+def main():
+    print(find_all_duplicates([3, 4, 4, 5, 5]))
+    print(find_all_duplicates([5, 4, 7, 2, 3, 5, 3]))
+
+
+if __name__ == '__main__':
+    main()
