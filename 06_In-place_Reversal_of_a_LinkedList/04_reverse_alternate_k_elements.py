566737a3deb11070cab7a739c2daa4a5ddc05281
shash873 <shash873@gmail.com>
2021-05-30 12:29:15 -0700

    Reverse alternating K-element Sub-list

---
+++ 06_In-place_Reversal_of_a_LinkedList/04_reverse_alternate_k_elements.py
@@ -0,0 +1,68 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def print_list(self):
+        temp = self
+        while temp is not None:
+            print(temp.value, end=" ")
+            temp = temp.next
+        print()
+
+
+# Time O(N) | Space O(1)
+def reverse_alternate_k_elements(head, k):
+    if k <= 1 or head is None:
+        return head
+
+    curr, prev = head, None
+    while True:
+        last_node_of_previous_part = prev
+        last_node_of_sub_list = curr
+
+        i = 0
+        while curr and i < k:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+            i += 1
+
+        if last_node_of_previous_part is not None:
+            last_node_of_previous_part.next = prev
+        else:
+            head = prev
+
+        last_node_of_sub_list.next = curr
+
+        i = 0
+        while curr and i < k:
+            prev = curr
+            curr = curr.next
+            i += 1
+        if curr is None:
+            break
+
+    return head
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    head.next.next.next.next.next.next = Node(7)
+    head.next.next.next.next.next.next.next = Node(8)
+
+    print("Nodes of original LinkedList are: ", end='')
+    head.print_list()
+    result = reverse_alternate_k_elements(head, 2)
+    print("Nodes of reversed LinkedList are: ", end='')
+    result.print_list()
+
+
+if __name__ == '__main__':
+    main()
