f2439f1681ccec5454bb6f82f6431a7056aa09cd
shash873 <shash873@gmail.com>
2021-05-30 08:24:37 -0700

    LinkedList Cycle Length

---
+++ 03_Fast_and_Slow_Pointers/02_cycle_length.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+# Time O(N) | Space O(1)
+def find_cycle_length(head):
+    slow, fast = head, head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+        if slow == fast:
+            return calculate_cycle_length(slow)
+    return 0
+
+
+def calculate_cycle_length(slow):
+    current = slow
+    cycle_length = 0
+    while True:
+        current = current.next
+        cycle_length += 1
+        if current == slow:
+            break
+    return cycle_length
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    head.next.next.next.next.next.next = head.next.next
+    print("LinkedList cycle length: " + str(find_cycle_length(head)))
+
+    head.next.next.next.next.next.next = head.next.next.next
+    print("LinkedList cycle length: " + str(find_cycle_length(head)))
+
+
+if __name__ == '__main__':
+    main()
