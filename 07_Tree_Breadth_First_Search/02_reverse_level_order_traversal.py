f25651312d843fda2d2eb48f178de1e3fb311ff4
shash873 <shash873@gmail.com>
2021-05-30 19:14:31 -0700

    Reverse Level Order Traversal

---
+++ 07_Tree_Breadth_First_Search/02_reverse_level_order_traversal.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+# Time O(N) | Space O(N)
+def reverse_level_order_traversal(root):
+    result = deque([])
+    if not root:
+        return result
+
+    queue = deque([root])
+    while queue:
+        level_vals = []
+        level_length = len(queue)
+        for _ in range(level_length):
+            node = queue.pop()
+            level_vals.append(node.val)
+            if node.left:
+                queue.appendleft(node.left)
+            if node.right:
+                queue.appendleft(node.right)
+        result.appendleft(level_vals)
+
+    return list(result)
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Reverse level order traversal: " + str(reverse_level_order_traversal(root)))
+
+
+if __name__ == '__main__':
+    main()
