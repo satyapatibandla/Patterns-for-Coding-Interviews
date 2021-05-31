d53ebd1da313f7ddc64d0c8b5eaf92806591f334
shash873 <shash873@gmail.com>
2021-05-31 07:28:40 -0700

    Left View of a Binary Tree

---
+++ 07_Tree_Breadth_First_Search/12_tree_left_view.py
@@ -0,0 +1,47 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+
+
+# Time O(N) | Space O(N)
+def tree_left_view(root):
+    result = []
+    if not root:
+        return result
+
+    queue = deque([root])
+
+    while queue:
+        level_length = len(queue)
+        for i in range(level_length):
+            node = queue.pop()
+            if i == 0:
+                result.append(node)
+            if node.left:
+                queue.appendleft(node.left)
+            if node.right:
+                queue.appendleft(node.right)
+
+    return result
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    root.left.left.left = TreeNode(3)
+    result = tree_left_view(root)
+    print("Tree left view: ")
+    for node in result:
+        print(str(node.val) + " ", end='')
+
+
+if __name__ == '__main__':
+    main()
